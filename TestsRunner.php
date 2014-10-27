ee7fca6b95dcbbec9f4402c2c14c7af41e8bf8d9
OZ <normandiggs@gmail.com>
2014-10-27 19:01:55 +0300

    TestsRunner now scan sub-directories

--- TestsRunner.php
+++ TestsRunner.php
@@ -9,6 +9,33 @@ class TestsRunner
       trigger_error('No folder specified', E_USER_WARNING);
       return false;
     }
+    $tests          = array();
+    $classes_before = get_declared_classes();
+    if (!$this->includeFolder($folder, $prefix, $suffix)) {
+      return false;
+    }
+    $classes_after = get_declared_classes();
+    $classes       = array_diff($classes_after, $classes_before);
+    if (!empty($classes)) {
+      foreach ($classes as $class) {
+        try {
+          /** @var ClassTest $Test */
+          $Test = new $class();
+        }
+        catch (\Exception $E) {
+          continue;
+        }
+        if (($Test instanceof ClassTest) && get_class($Test) !== 'Jamm\\Tester\\ClassTest') {
+          $Test->RunTests();
+          $tests = array_merge($tests, $Test->getTests());
+        }
+      }
+    }
+    return $tests;
+  }
+
+  protected function includeFolder($folder, $prefix, $suffix)
+  {
     $files = scandir($folder);
     if (empty($files)) {
       trigger_error('Can\'t scan folder '.$folder, E_USER_WARNING);
@@ -24,13 +51,16 @@ class TestsRunner
       trigger_error('No files in folder '.$folder, E_USER_WARNING);
       return false;
     }
-    $tests          = array();
-    $classes_before = get_declared_classes();
     foreach ($files as $file) {
       $path = realpath($folder).'/'.$file;
       if (!file_exists($path) || !is_readable($path)) {
         continue;
       }
+      if (is_dir($path) && $file != '.' && $file != '..' && $file != './') {
+        $this->includeFolder($path, $prefix, $suffix);
+        continue;
+      }
+
       if ($prefix) {
         if (stripos($file, $prefix) !== 0) {
           continue;
@@ -43,23 +73,6 @@ class TestsRunner
       }
       include_once $path;
     }
-    $classes_after = get_declared_classes();
-    $classes       = array_diff($classes_after, $classes_before);
-    if (!empty($classes)) {
-      foreach ($classes as $class) {
-        try {
-          /** @var ClassTest $Test */
-          $Test = new $class();
-        }
-        catch (\Exception $E) {
-          continue;
-        }
-        if (($Test instanceof ClassTest) && get_class($Test)!=='Jamm\\Tester\\ClassTest') {
-          $Test->RunTests();
-          $tests = array_merge($tests, $Test->getTests());
-        }
-      }
-    }
-    return $tests;
+    return true;
   }
 }
