35a2aeafb3cffd81692f441fb7cd359ef38bcec1
OZ <normandiggs@gmail.com>
2014-10-27 12:31:11 +0300

    Update code style

--- ErrorCatcher.php
+++ ErrorCatcher.php
@@ -1,73 +1,73 @@
 <?php
 namespace Jamm\Tester;
+
 class ErrorCatcher
 {
-	private $handler_changed = false;
-	/** @var Error[] */
-	private $errors = array();
-	private $errors_types = E_ALL;
-	/** @var DebugTracer */
-	private $debug_tracer;
+  private $handler_changed = false;
+  /** @var Error[] */
+  private $errors = array();
+  private $errors_types = E_ALL;
+  /** @var DebugTracer */
+  private $debug_tracer;
 
-	public function __construct()
-	{
-		$this->errors_types = E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED;
-	}
+  public function __construct()
+  {
+    $this->errors_types = E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED;
+  }
 
-	public function setUp()
-	{
-		if (set_error_handler(array($this, 'errorHandler'), $this->errors_types))
-		{
-			$this->handler_changed = true;
-			return true;
-		}
-		else return false;
-	}
+  public function setUp()
+  {
+    if (set_error_handler(array($this, 'errorHandler'), $this->errors_types)) {
+      $this->handler_changed = true;
+      return true;
+    }
+    else return false;
+  }
 
-	public function getErrors()
-	{
-		return $this->errors;
-	}
+  public function getErrors()
+  {
+    return $this->errors;
+  }
 
-	/**
-	 * @return Error
-	 */
-	protected function getNewErrorObject()
-	{
-		/** Cake is a lie */
-		return new Error();
-	}
+  public function errorHandler($error_code, $error_message, $filepath = '', $line = 0)
+  {
+    $error = $this->getNewErrorObject();
+    $error->setCode($error_code);
+    $error->setMessage($error_message);
+    $error->setFilepath($filepath);
+    $error->setLine($line);
+    $error->setDebugTrace($this->getCurrentBacktrace());
+    $this->errors[] = $error;
+  }
 
-	public function errorHandler($error_code, $error_message, $filepath = '', $line = 0)
-	{
-		$error = $this->getNewErrorObject();
-		$error->setCode($error_code);
-		$error->setMessage($error_message);
-		$error->setFilepath($filepath);
-		$error->setLine($line);
-		$error->setDebugTrace($this->getCurrentBacktrace());
-		$this->errors[] = $error;
-	}
+  public function __destruct()
+  {
+    if ($this->handler_changed) restore_error_handler();
+  }
 
-	private function getCurrentBacktrace()
-	{
-		if (empty($this->debug_tracer)) $this->debug_tracer = new DebugTracer();
-		return $this->debug_tracer->getCurrentBacktrace();
-	}
+  public function hasErrors()
+  {
+    return !empty($this->errors);
+  }
 
-	public function __destruct()
-	{
-		if ($this->handler_changed) restore_error_handler();
-	}
+  public function setHandledErrorsTypes($errors_types)
+  {
+    $this->errors_types = $errors_types;
+  }
 
-	public function hasErrors()
-	{
-		return !empty($this->errors);
-	}
+  /**
+   * @return Error
+   */
+  protected function getNewErrorObject()
+  {
+    /** Cake is a lie */
+    return new Error();
+  }
 
-	public function setHandledErrorsTypes($errors_types)
-	{
-		$this->errors_types = $errors_types;
-	}
+  private function getCurrentBacktrace()
+  {
+    if (empty($this->debug_tracer)) $this->debug_tracer = new DebugTracer();
+    return $this->debug_tracer->getCurrentBacktrace();
+  }
 
 }
--- Test.php
+++ Test.php
@@ -1,80 +1,79 @@
 <?php
 namespace Jamm\Tester;
+
 class Test
 {
-	/** @var \Exception */
-	private $exception;
-	private $errors;
-	/** @var Assertion[] */
-	private $assertions = array();
-	private $successful = true;
-	private $name;
+  /** @var \Exception */
+  private $exception;
+  private $errors;
+  /** @var Assertion[] */
+  private $assertions = array();
+  private $successful = true;
+  private $name;
 
-	public function getException()
-	{
-		return $this->exception;
-	}
+  public function getException()
+  {
+    return $this->exception;
+  }
 
-	public function addAssertion(Assertion $assertion)
-	{
-		$this->assertions[] = $assertion;
-		if (!$assertion->isSuccessful()) $this->successful = false;
-	}
+  public function setException(\Exception $exception)
+  {
+    if (!empty($exception)) {
+      $this->exception  = $exception;
+      $this->successful = false;
+    }
+  }
 
-	public function getAssertions()
-	{
-		return $this->assertions;
-	}
+  public function addAssertion(Assertion $assertion)
+  {
+    $this->assertions[] = $assertion;
+    if (!$assertion->isSuccessful()) $this->successful = false;
+  }
 
-	public function setException(\Exception $exception)
-	{
-		if (!empty($exception))
-		{
-			$this->exception  = $exception;
-			$this->successful = false;
-		}
-	}
+  public function getAssertions()
+  {
+    return $this->assertions;
+  }
 
-	public function hasException()
-	{
-		return !empty($this->exception);
+  public function hasException()
+  {
+    return !empty($this->exception);
 
-	}
+  }
 
-	/**
-	 * @return Error[]
-	 */
-	public function getErrors()
-	{
-		return $this->errors;
-	}
+  /**
+   * @return Error[]
+   */
+  public function getErrors()
+  {
+    return $this->errors;
+  }
 
-	public function setErrors($errors)
-	{
-		if (!empty($errors))
-		{
-			$this->errors     = $errors;
-			$this->successful = false;
-		}
-	}
+  public function setErrors($errors)
+  {
+    if (!empty($errors)) {
+      $this->errors     = $errors;
+      $this->successful = false;
+    }
+  }
 
-	public function isSuccessful()
-	{
-		return $this->successful;
-	}
+  public function isSuccessful()
+  {
+    return $this->successful;
+  }
 
-	public function setSuccessful($successful = true)
-	{
-		$this->successful = $successful;
-	}
+  public function setSuccessful($successful = true)
+  {
+    $this->successful = $successful;
+  }
 
-	public function getName()
-	{
-		return $this->name;
-	}
+  public function getName()
+  {
+    return $this->name;
+  }
 
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
 }
